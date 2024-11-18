735171fd0eb936af10eccdd63b15dff699787982
MicheleFumi <michele.fumi96@gmail.com>
2024-11-18 16:51:53 +0100

    bug fix

--- src/Components/AppMain.jsx
+++ src/Components/AppMain.jsx
@@ -55,21 +55,19 @@ export default function AppMain() {
                         <input type="text" className="form-control" placeholder="Inserisci un titolo" aria-label="Inserisci un titolo " aria-describedby="button-addon2" value={newTitles} onChange={e => setNewTitles(e.target.value)} />
                         <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Aggiungi</button>
                     </div>
-                    <h2 >Titoli Di Oggi</h2>
-                    <ul className="list-group mt-4  ">
-                        {titles.map((title, index) => <li className="list-group-item d-flex justify-content-between mx-4" key={index}>{title}
-                            <div>
-                                <button className='btn btn-warning me-2' onClick={handleChangeTitle} data-index={index}>Cambia</button>
-                                <button className='btn btn-danger' onClick={handleRemoveTitle} data-index={index}>Rimuovi</button>
-                            </div>
-
-
-                        </li>)}
-
-                    </ul>
-
-
                 </form>
+                <h2 >Titoli Di Oggi</h2>
+                <ul className="list-group mt-4  ">
+                    {titles.map((title, index) => <li className="list-group-item d-flex justify-content-between mx-4" key={index}>{title}
+                        <div>
+                            <button className='btn btn-warning me-2' onClick={handleChangeTitle} data-index={index}>Cambia</button>
+                            <button className='btn btn-danger' onClick={handleRemoveTitle} data-index={index}>Rimuovi</button>
+                        </div>
+
+
+                    </li>)}
+
+                </ul>
             </div>
 
         </main>
